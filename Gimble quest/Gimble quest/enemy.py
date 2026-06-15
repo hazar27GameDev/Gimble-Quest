d55c34a5a5f5f849448ef177e0170d300057105b
Kobi-Nichols <kobi.nichols@education.nsw.gov.au>
2026-06-15 15:10:24 +1000

    Enemy Class 15/06/26
    Added just simple things to the enemy class like attack defence and HP.

--- Gimble quest/Gimble quest/enemy.py
+++ Gimble quest/Gimble quest/enemy.py
@@ -1,5 +1,18 @@
-class my_class(object):
-    pass
+class Enemy():
+    def __init__ (self, name, health, attack, defence):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defence = defence
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self):
+        return self.health > 0
+    
 
 
 
