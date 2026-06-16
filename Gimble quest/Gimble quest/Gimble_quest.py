bc3d81ed0b380d5a255ae665b821f4d335ced411
hazar27GameDev <harrison.imre@gmail.com>
2026-06-17 09:33:20 +1000

    main menu func and player class
    i will have to deal with a merge conflict cause i clicked save all😭

--- Gimble quest/Gimble quest/Gimble_quest.py
+++ Gimble quest/Gimble quest/Gimble_quest.py
@@ -1,1 +1,8 @@
+#Main menu
+def MainMenu():
+    print("Welcome to Epic RPG Game")
 
+
+    print("This is a tuff RPG made by the epic guys named Harry, Kobi, Jacobi")
+
+MainMenu()
--- Gimble quest/Gimble quest/Player.py
+++ Gimble quest/Gimble quest/Player.py
@@ -1,4 +1,15 @@
-class player(object):
-    print("idk")
+class Player:
 
+    def __init__(self, name, ClassType, health, stamina):
+        self.name = name
+        self.ClassType = ClassType
+        self.health = health
+        self.stamina = stamina
 
+    def PlayerInfo(self):
+
+        print("Player Info: ")
+        print(f"Name: {self.name}")
+        print(f"Class: {self.ClassType}")
+        print(f"Health: {self.health}")
+        print(f"Stamina: {self.stamina}")
