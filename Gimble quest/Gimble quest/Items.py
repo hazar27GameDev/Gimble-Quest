763ee599ccfc5b4e95ac23b64629165a4f0817b5
JRamplin12 <jacobi.perfrementramplin@education.nsw.gov.au>
2026-06-17 09:53:10 +1000

    item class
    added the basics to the item class

--- Gimble quest/Gimble quest/Items.py
+++ Gimble quest/Gimble quest/Items.py
@@ -1,6 +1,15 @@
-class my_class(object):
-    pass
+class item:
+    def __init__(self, items, damage, heal, armor):
+        self.items = items
+        self.damage = damage
+        self.heal = heal
+        self.armor = armor
 
+    def AddHeal(self):
+        self.heal = 10
 
+    def AddArmor(self):
+        self.armor = 5
 
-
+    def AddDamage(self):
+        self.damage = 2
