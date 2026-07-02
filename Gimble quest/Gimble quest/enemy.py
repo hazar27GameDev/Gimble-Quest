572536a37923ce8db641ae525fe54a40b48932ad
hazar27GameDev <harrison.imre@gmail.com>
2026-07-02 12:13:21 +1000

    revert finnal

--- Gimble quest/Gimble quest/enemy.py
+++ Gimble quest/Gimble quest/enemy.py
@@ -94,7 +94,7 @@ class Slime(Enemy):
     Represents a Slime Inherited from Enemy.
     Author: Kobi Nichols
     Date: 25/06/2026
-    Parameters: attack, health, name
+    Parameters: none
     Returns: Enemy object
     """
     # Slime information
@@ -119,7 +119,7 @@ class Dragon(Enemy):
     Represents a Dragon Inherited from Enemy.
     Author: Kobi Nichols
     Date: 25/06/2026
-    Parameters: Dragon name, health, attack
+    Parameters: none
     Returns: Enemy object
     """
 
